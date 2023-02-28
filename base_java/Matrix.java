96517c397484cf4591997d0a51908713bfe33725
Jing Qian <jqian30@stuy.edu>
2023-02-28 15:27:52 -0500

    aaaaa

--- base_java/Matrix.java
+++ base_java/Matrix.java
@@ -45,6 +45,7 @@ public class Matrix {
     calling matrix to be the product
    ====================*/
   public void mult(Matrix a) {
+    assert n_columns() == a.n_columns();
   }//mult
 
 
@@ -70,4 +71,41 @@ public class Matrix {
 
     return s;
   }
+
+  public double[] getCol(int i) {
+    return m.get(i);
+  }
+
+  public double[] getRow(int index) {
+    double[] d = new double[m.size()];
+    for (int i = 0; i < d.length; i++) {
+      d[i] = m.get(i)[index];
+    }
+    return d;
+  }
+
+  public int n_columns() {
+    return m.size();
+  }
+
+  public static void main(String[] args) {
+    System.out.println("make matrix:");
+    Matrix owo = new Matrix();
+    owo.addColumn(1, 2, 3);
+    owo.addColumn(5, 6, 7);
+    System.out.println(owo);
+    System.out.println();
+
+    System.out.println("get row 0");
+    System.out.println(Arrays.toString(owo.getRow(0)));
+    System.out.println("get col 0");
+    System.out.println(Arrays.toString(owo.getCol(0)));
+    System.out.println();
+
+    System.out.println("ident");
+    Matrix id = new Matrix();
+    id.ident();
+    System.out.println(id);
+  }
+
 }//Matrix
