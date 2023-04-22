68f812a57207f370f14a4fec28ed9c01e87d179c
Jing Qian <jqian30@stuy.edu>
2023-04-22 15:03:47 -0400

    add and draw polygons probably

--- PolygonMatrix.java
+++ PolygonMatrix.java
@@ -3,8 +3,8 @@ import java.awt.*;
 
 public class PolygonMatrix extends Matrix {
 
-  public void addBox( double x, double y, double z,
-                       double width, double height, double depth ) {
+  public void addBox( double x, double y, double z, double width, double height, double depth ) {
+    
   }//addBox
 
   public void addSphere( double cx, double cy, double cz,
@@ -50,8 +50,7 @@ public class PolygonMatrix extends Matrix {
 
   }//addTorus
 
-  private Matrix generateTorus(double cx, double cy, double cz,
-                               double r0, double r1, int steps ) {
+  private Matrix generateTorus(double cx, double cy, double cz, double r0, double r1, int steps ) {
 
     Matrix points = new Matrix();
     int circle, rotation, rot_start, rot_stop, circ_start, circ_stop;
@@ -87,11 +86,28 @@ public class PolygonMatrix extends Matrix {
   public void addPolygon(double x0, double y0, double z0,
                          double x1, double y1, double z1,
                          double x2, double y2, double z2) {
-
+    //new Polygon(new double[] {x0, y0, z0}, new double[] {x1, y1, z1}, new double[] {x2, y2, z2}, co);
+    m.add(new double[] {x0, y0, z0, 1});
+    m.add(new double[] {x1, y1, z1, 1});
+    m.add(new double[] {x2, y2, z2, 1});
   }//addColumn
 
   public void drawPolygons(Screen s, Color c) {
+    assert m.size() % 3 == 0;
+    if (m.size() < 3) {
+      System.out.println("need at least 3 points to draw polygons");
+      return;
+    }
 
+    for (int i = 0; i < m.size() - 2; i += 3) {
+      double[] p0 = m[i];
+      double[] p1 = m[i + 1];
+      double[] p2 = m[i + 2];
+
+      s.drawLine((int)p0[0], (int)p0[1], (int)p1[0], (int)p1[1], c);
+      s.drawLine((int)p0[0], (int)p0[1], (int)p2[0], (int)p2[1], c);
+      s.drawLine((int)p1[0], (int)p1[1], (int)p2[0], (int)p2[1], c);
+    }
   }//drawPloygons
 
 }//class PolygonMatrix
